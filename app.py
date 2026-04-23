ca08a8c12a53fbc8ec01c84d2222e0d50b702372
karlzammit89 <karlzammit89@gmail.com>
2026-04-23 09:10:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if mode == "Schedule":
 
             for game in games:
                 st.write(
-                    f"{game['gamePk']} | 🏒 {game['matchup']} | 🕒 {game['time']} ET | {game['state']}"
+                    f"{game['gamePk']} | 🏒 {game['matchup']} | 🕒 {game['time']} (ET)}"
                 )
 
         else:
